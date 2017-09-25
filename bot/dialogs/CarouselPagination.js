9fe95ae529c1d6b7919e14c2d80efaf6fce20f64
Akhil <akhil2.new@gmail.com>
2017-09-25 21:36:04 +0530

    changed CPN,PS,PROD.js

--- bot/dialogs/CarouselPagination.js
+++ bot/dialogs/CarouselPagination.js
@@ -1,4 +1,12 @@
 var builder = require('botbuilder');
+var Client = require('node-rest-client').Client;
+var solr = require('solr-client');
+var SolrQueryBuilder = require('solr-query-builder');
+var SolrNode = require('solr-node');
+
+var client = new Client();
+
+var client1 = solr.createClient('54.158.112.215', '8080', 'ecommerce_1');
 
 var defaultSettings = {
     showMoreTitle: 'title_show_more',
@@ -7,7 +15,6 @@ var defaultSettings = {
     pageSize: 10,
     unknownOption: 'unknown_option'
 };
-
 module.exports = {
     create: function (getPageFunc, getItemFunc, itemToCardFunc, settings) {
         // parameter validation
@@ -47,7 +54,7 @@ module.exports = {
         };
 
         // return dialog handler funciton
-        return function (session, args, next) {
+        return  function (session, args, next) {
             var pageNumber = session.dialogData.pageNumber || 1;
             var input = session.message.text;
             var selectPrefix = session.gettext(settings.selectTemplate);
@@ -58,6 +65,7 @@ module.exports = {
                 // Validate selection
                 var selectedName = input.substring(selectPrefix.length);
                 getItemFunc(selectedName).then(function (selectedItem) {
+                    
                     if (!selectedItem) {
                         return session.send(settings.unknownOption);
                     }
@@ -70,6 +78,240 @@ module.exports = {
                 });
 
                 return;
+            }
+            else if (session.message.text){
+                //session.send(session.dialogData.allProducts)
+                return session.beginDialog('/')
+            }
+            else if (session.message.value){
+                function getSearchQuery(){
+                    //session.send("ITS BEGINNIG HEREEEEEEEeee"),
+                    allProducts=[]
+                    url_te = "http://34.201.62.199:7777/nlpapi/tri?text=" + session.message.text;
+                    search_qry=[]
+                    var args =
+                    {
+                        data: { test: "hello" },
+                        headers: { "Content-Type": "application/json" }
+                    };
+                    var client = new Client();
+                    client.post
+                    (url_te, args, function (data, response) {
+                        // console.log("The length of the variable data is " + data.length);
+                        //Handles when triple extarction yields null results
+                        if (data.length == 0) {
+                            search_qry.push(session.message.text);
+                            // session.send("You wanted to buy " + msg + ". Here are the results." + search_qry);
+                        }
+                
+                        //Handles when triple extraction yields results
+                        else {
+                            reslen = Object.keys(data).length;
+        
+                            for (var i = 0; i < reslen; i++) {
+                                search_qry.push(data[i]['object']);
+                            }
+                            //console.log(search_qry);
+                        }
+                        search_qry = session.dialogData.search_qry
+                        final1()   
+                    })
+                    
+                }
+                function final2(){
+                    //console.log("I AM HERE FINAL22")
+                    //console.log(query)
+                    client1.search
+                    (getQuery(search_qry), function (err, obj) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log("I AM HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+                            resultsobj = obj.response;
+                            console.log(resultsobj)
+                            console.log("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy")
+                            console.log(resultsobj.docs[1])
+                            console.log(resultsobj.numFound)
+                            console.log(typeof(resultsobj.docs))
+                            console.log(resultsobj.docs)
+                            
+                            for(var i =0; i < resultsobj.numFound; i++){
+                                allProducts.push({
+                                    name:resultsobj.docs[i].title.toString(),
+                                    // name:"Algonox Product " + i,
+                                    imageUrl:'',
+                                    price:parseFloat((resultsobj.docs[i]['formattedPrice']).slice(1)),
+                                })
+                            }
+                            final3()
+                        }
+                    })
+                }
+                function final1(){
+                    query = getQuery(search_qry)
+                    final2()
+                }
+                function final3(){
+                    session.dialogData.allProducts=allProducts
+
+                    // retrieve from service and send items
+            getPageFunc(pageNumber, settings.pageSize).then(function (pageResult) {
+                // save current page number
+                session.dialogData.pageNumber = pageNumber;
+
+                // items carousel
+                var cards = pageResult.items
+                    .map(itemToCardFunc)
+                    .map(function (cardData) { return asCard(session, cardData); });
+                var message = new builder.Message(session)
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(cards);
+                session.send(message);
+
+                // more items link
+                if (pageResult.totalCount > pageNumber * settings.pageSize) {
+                    var moreCard = new builder.HeroCard(session)
+                        .title(settings.showMoreTitle)
+                        .buttons([
+                            builder.CardAction.imBack(session, session.gettext(settings.showMoreValue), settings.showMoreValue)
+                        ]);
+                    session.send(new builder.Message(session).addAttachment(moreCard));
+                }
+
+
+                var msg = new builder.Message(session).addAttachment({
+                    contentType: "application/vnd.microsoft.card.adaptive",
+                    content:{
+                        "type": "AdaptiveCard",
+                        "version": "0.5",
+                        "body": [
+                            {
+                                "type":"TextBlock",
+                                "text":"Specifications",
+                                "weight":"bolder"
+                            },
+                            {
+                                "type": "Input.ChoiceSet",
+                                "id": "myColor",
+                                "style": "compact",
+                                "placeholder":"Choose Color",
+                                "isMultiSelect": false,
+                                "choices":[
+                                    {
+                                        "title": "Red",
+                                        "value": "Red"
+                                    },
+                                    {
+                                        "title": "Black",
+                                        "value": "Black"
+                                    },
+                                    {
+                                        "title": "Green",
+                                        "value": "Green"
+                                    }
+                                ]
+                            
+                            },
+                             {
+                                "type": "Input.Toggle",
+                                "id": "IsSale",
+                                "title": "Sale?",
+                                "valueOn": "true",
+                                "valueOff": "false"
+                            
+                            },
+                           
+                            {
+                                "type": "Input.Toggle",
+                                "id": "IsReturnPolicy",
+                                "title": "ReturnPolicy?",
+                                "valueOn": "true",
+                                "valueOff": "false"
+                            
+                            }
+                            ],
+                            
+                        "actions": [
+                            {
+                                "type": "Action.Submit",
+                                "title": "Search",
+                                "data": {
+                                    
+                                }            
+                            }
+                        ]
+                    }
+                })
+                session.send(msg)
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                }
+                function getQuery(search_qry) {
+                   // var client = solr.createClient('54.158.112.215','8080','ecommerce_1');
+                   // search_qry = ["shoes","shirts"]
+                    
+                    //True coonversion of array into string
+                    console.log(search_qry)
+                    stringsearched = JSON.stringify(search_qry);
+                    
+                    //stringsearched=[]
+                    // console.log(typeof(stringsearched))
+                    // console.log("The string searched is " + stringsearched)
+                    
+                    inko = search_qry.toString();
+                    console.log("jhdvsjvs" + inko)
+
+                    filterobj = session.message.value;
+                    
+                    if(filterobj.myColor === "")
+                    {
+                        filterobj.myColor = "*";
+                    }
+                    
+                    //For manipulating sale filter
+                    if (filterobj.IsSale === "true")
+                    {
+                        filterobj.IsSale = 'y'
+                    }
+                    else if(filterobj.IsSale === "false")
+                    {
+                        filterobj.IsSale = 'n'
+                    }
+                    
+                    //For manipulating returnpolicy filter
+                    if (filterobj.IsReturnPolicy === "true")
+                    {
+                        filterobj.IsReturnPolicy = 'y'
+                    }
+                    else if(filterobj.IsReturnPolicy === "false")
+                    {
+                        filterobj.IsReturnPolicy = 'n'
+                    }
+                    
+                
+                    var query = client1.query().q({title:inko}).matchFilter('color',filterobj.myColor).matchFilter('sale',filterobj.IsSale).matchFilter('isReturnPolicy',filterobj.IsReturnPolicy).start(0).rows(10000)
+        
+                    console.log(query)
+                    
+                    return (query)
+}
+                getSearchQuery()
             }
 
             // retrieve from service and send items
@@ -95,6 +337,72 @@ module.exports = {
                         ]);
                     session.send(new builder.Message(session).addAttachment(moreCard));
                 }
+
+
+                var msg = new builder.Message(session).addAttachment({
+                    contentType: "application/vnd.microsoft.card.adaptive",
+                    content:{
+                        "type": "AdaptiveCard",
+                        "version": "0.5",
+                        "body": [
+                            {
+                                "type":"TextBlock",
+                                "text":"Specifications",
+                                "weight":"bolder"
+                            },
+                            {
+                                "type": "Input.ChoiceSet",
+                                "id": "myColor",
+                                "style": "compact",
+                                "placeholder":"Choose Color",
+                                "isMultiSelect": false,
+                                "choices":[
+                                    {
+                                        "title": "Red",
+                                        "value": "Red"
+                                    },
+                                    {
+                                        "title": "Black",
+                                        "value": "Black"
+                                    },
+                                    {
+                                        "title": "Green",
+                                        "value": "Green"
+                                    }
+                                ]
+                            
+                            },
+                             {
+                                "type": "Input.Toggle",
+                                "id": "IsSale",
+                                "title": "Sale?",
+                                "valueOn": "true",
+                                "valueOff": "false"
+                            
+                            },
+                           
+                            {
+                                "type": "Input.Toggle",
+                                "id": "IsReturnPolicy",
+                                "title": "ReturnPolicy?",
+                                "valueOn": "true",
+                                "valueOff": "false"
+                            
+                            }
+                            ],
+                            
+                        "actions": [
+                            {
+                                "type": "Action.Submit",
+                                "title": "Search",
+                                "data": {
+                                    
+                                }            
+                            }
+                        ]
+                    }
+                })
+                session.send(msg)
             });
         };
     }
--- services/products.js
+++ services/products.js
@@ -1,47 +1,116 @@
 var _ = require('lodash');
 var Promise = require('bluebird');
+var Client = require('node-rest-client').Client;
+var solr = require('solr-client');
+var SolrQueryBuilder = require('solr-query-builder');
+var SolrNode = require('solr-node');
+/*
+var client = new Client();
+allProducts = []
+url_te = "http://34.201.62.199:7777/nlpapi/tri?text=" + "I love shoes";
+var client1 = solr.createClient('54.158.112.215', '8080', 'ecommerce');
+function getSearchQuery(){
+    search_qry=[]
+    var args =
+    {
+        data: { test: "hello" },
+        headers: { "Content-Type": "application/json" }
+    };
+    var client = new Client();
+    client.post
+    (url_te, args, function (data, response) {
+        // console.log("The length of the variable data is " + data.length);
+        //Handles when triple extarction yields null results
+        if (data.length == 0) {
+            search_qry.push(msg);
+            //session.send("You wanted to buy " + msg + ". Here are the results.");
+        }
 
-var allCategories = _.times(5)
-    .map(function (i) {
-        return {
-            name: 'Flower ' + (i + 1),
-            imageUrl: 'https://placeholdit.imgix.net/~text?txtsize=48&txt=Flower%20' + (i + 1) + '&w=640&h=330'
-        };
-    });
+        //Handles when triple extraction yields results
+        else {
+            reslen = Object.keys(data).length;
 
-var allProducts = _.times(17)
-    .map(function (i) {
-        return {
-            name: 'Bouquet ' + (i + 1) + '\u2122',
-            imageUrl: 'https://placeholdit.imgix.net/~text?txtsize=48&txt=Bouquet%20' + (i + 1) + '&w=640&h=330',
-            price: Math.floor(Math.random() * 100) + 10 + .99
-        };
-    });
+            for (var i = 0; i < reslen; i++) {
+                search_qry.push(data[i]['object']);
+            }
+            console.log(search_qry);
+        }
+        final1()   
+    })
+    
+}
 
-var productsService = {
-    // Categories
-    getCategories: function (pageNumber, pageSize) {
-        return pageItems(pageNumber, pageSize, allCategories);
-    },
+function final2(){
+    console.log("I AM HERE FINAL22")
+    console.log(query)
+    client1.search
+    (getQuery(search_qry), function (err, obj) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            resultsobj = obj.response;
+            //console.log(resultsobj)
+            //console.log(resultsobj.docs[1]['title'].toString())
+            for(var i =0; i < 10; i++){
+                allProducts.push(i)
+            }
+            console.log("YYYYYYYYYYYYYY")
+            final3()
+        }
+    })
+}
+function final1(){
+    console.log("I AM HERE FIRST FINAL")
+    console.log(search_qry)
+    console.log((getQuery(search_qry)))
+    query = getQuery(search_qry)
+    final2()
+}
+function final3(){
+    console.log("HERE WE CAME THE SOLUTION IS: ")
+    console.log(allProducts)
+}
+function getQuery(search_qry) {
+    //Creating connection to Solr
+    var qb = new SolrQueryBuilder();
+    
+    // var opt = {title:['guestbook','heater']};
 
-    // Get Single Category
-    getCategory: function (categoryName) {
-        var category = _.find(allCategories, ['name', categoryName]);
-        return Promise.resolve(category);
-    },
+    //True coonversion of array into string
+    stringsearched = JSON.stringify(search_qry);
+    //stringsearched=[]
+    // console.log(typeof(stringsearched))
+    // console.log("The string searched is " + stringsearched)
 
-    // Products
-    getProducts: function (categoryName, pageNumber, pageSize) {
+    //Defines the properties and gives the values to be searched in those properties
+    var opt = { title: [stringsearched] };
+
+    //Creates the query with multiple search terms retrieved from stringssearched
+    if (opt.title) qb.where('title').in(opt.title);
+
+    //Connects to the Solr server and passes the query.            
+    var query = client1.createQuery().q(qb.build()).start(0).rows(3);
+    return (query)
+}
+
+*/
+
+productsService = {
+    //     Products
+
+    getProducts: function (pageNumber, pageSize, allProducts) {
         return pageItems(pageNumber, pageSize, allProducts);
     },
 
     // Get Single Product
-    getProduct: function (productName) {
+    getProduct: function (productName, allProducts) {
         var product = _.find(allProducts, ['name', productName]);
         return Promise.resolve(product);
     }
+    
+    //getAlll: getAll2
 };
-
 // helpers
 function pageItems(pageNumber, pageSize, items) {
     var pageItems = _.take(_.drop(items, pageSize * (pageNumber - 1)), pageSize);
